6c317ecf242049c1541a14fa45b309ff4f935a19
ansagan <islamgaliev@mail.ru>
2022-01-23 17:44:34 +0500

    logic: Realizes infinite scroll for FlightCardList componenet

--- src/components/FlightList/FlightList.vue
+++ src/components/FlightList/FlightList.vue
@@ -1,6 +1,10 @@
 <template>
-  <ul class="flight-list">
-    <li v-for="flight in flights" :key="flight.id" class="flight-list-item">
+  <ul class="flight-list" ref="scrollComponent">
+    <li
+      v-for="flight in renderedList"
+      :key="flight.id"
+      class="flight-list-item"
+    >
       <flight-card :flight="flight" />
     </li>
   </ul>
@@ -16,6 +20,42 @@ export default {
     flights: {
       type: Array,
       required: true,
+    },
+  },
+  mounted() {
+    this.addMoreItem();
+
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  unmounted() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  data() {
+    return {
+      startIndex: 0,
+      endIndex: 6,
+      step: 6,
+      renderedList: [],
+    };
+  },
+  methods: {
+    addMoreItem() {
+      this.renderedList = this.renderedList.concat(
+        this.flights.slice(this.startIndex, this.endIndex),
+      );
+
+      this.startIndex += this.step;
+      this.endIndex += this.step;
+    },
+    handleScroll() {
+      const element = this.$refs.scrollComponent;
+
+      if (
+        element.getBoundingClientRect().bottom < window.innerHeight
+        && this.flights.length > this.endIndex + 1
+      ) {
+        this.addMoreItem();
+      }
     },
   },
 };
